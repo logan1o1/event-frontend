b9a2bce8caeddddd62f0db49bbe76b67bdcfc3c5
logan1o1 <saswatm2706@gmail.com>
2025-08-27 08:39:51 -0700

    deployed server url added

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://127.0.0.1:3000';
+export const API_BASE_URL = 'https://event-backend-rails.onrender.com';
 
 export const getAuthHeaders = (token: string) => ({
   'Content-Type': 'application/json',
